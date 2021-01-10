33d0860d2ca60dd67a1f6f5735a589678f2f3922
0x6d61 <0x6d61@gmail.com>
2021-01-10 21:41:11 +0900

    クロージャ実装

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ $primitive_fun_env = {
   :- => [:prim, lambda { |x, y| x - y }],
   :* => [:prim, lambda { |x, y| x * y }],
 }
-
+$global_env = [$primitive_fun_env]
 def list?(exp)
   exp.is_a?(Array)
 end
@@ -20,8 +20,8 @@ def cdr(list)
   list[1..-1]
 end
 
-def eval_list(exp)
-  exp.map { |e| _eval(e) }
+def eval_list(exp, env)
+  exp.map { |e| _eval(e, env) }
 end
 
 def immediate_val?(exp)
@@ -33,7 +33,15 @@ def num?(exp)
 end
 
 def apply(fun, args)
-  apply_primitive_fun(fun, args)
+  if primitive_fun?(fun)
+    apply_primitive_fun(fun, args)
+  else
+    lambda_apply(fun, args)
+  end
+end
+
+def primitive_fun?(exp)
+  exp[0] == :prim
 end
 
 def apply_primitive_fun(fun, args)
@@ -41,18 +49,89 @@ def apply_primitive_fun(fun, args)
   fun_val.call(*args)
 end
 
-def _eval(exp)
+def lookup_var(var, env)
+  alist = env.find { |alist| alist.key?(var) }
+  if alist == nil
+    raise "couldn't find value to variables: '#{var}'"
+  else
+    alist[var]
+  end
+end
+
+def extend_env(parameters, args, env)
+  alist = parameters.zip(args)
+  h = Hash.new
+  alist.each { |k, v| h[k] = v }
+  [h] + env
+end
+
+def eval_let(exp, env)
+  parameters, args, body = let_to_parameters_args_body(exp)
+  new_exp = [[:lambda, parameters, body]] + args
+  _eval(new_exp, env)
+end
+
+def let_to_parameters_args_body(exp)
+  [exp[1].map { |e| e[0] }, exp[1].map { |e| e[1] }, exp[2]]
+end
+
+def let?(exp)
+  exp[0] == :let
+end
+
+def eval_lambda(exp, env)
+  make_closure(exp, env)
+end
+
+def make_closure(exp, env)
+  parameters, body = exp[1], exp[2]
+  [:closure, parameters, body, env]
+end
+
+def lambda_apply(closure, args)
+  parameters, body, env = closure_to_parameters_body_env(closure)
+  new_env = extend_env(parameters, args, env)
+  _eval(body, new_env)
+end
+
+def closure_to_parameters_body_env(closure)
+  [closure[1], closure[2], closure[3]]
+end
+
+def _eval(exp, env)
   if not list?(exp)
     if immediate_val?(exp)
       exp
     else
-      lookup_primitive_fun(exp)
+      lookup_var(exp, env)
     end
   else
-    fun = _eval(car(exp))
-    args = eval_list(cdr(exp))
-    apply(fun, args)
+    if special_form?(exp)
+      eval_special_form(exp, env)
+    else
+      fun = _eval(car(exp), env)
+      args = eval_list(cdr(exp), env)
+      apply(fun, args)
+    end
   end
 end
 
-puts _eval([:+,1,2])
+def special_form?(exp)
+  lambda?(exp) or let?(exp)
+end
+
+def lambda?(exp)
+  exp[0] == :lambda
+end
+
+def eval_special_form(exp, env)
+  if lambda?(exp)
+    eval_lambda(exp, env)
+  elsif let?(exp)
+    eval_let(exp, env)
+  end
+end
+
+
+exp =  [[:lambda, [:x, :y], [:+, :x, :y]], 3, 2]
+puts _eval(exp, $global_env)
